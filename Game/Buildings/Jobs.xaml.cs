a44652769dc6f35a79b22d8f486a56157f97d7b8
Aggoh <A_Lyytinen@hotmail.com>
2016-04-12 10:47:52 +0300

    2.0

--- Game/Buildings/Jobs.xaml.cs
+++ Game/Buildings/Jobs.xaml.cs
@@ -26,8 +26,7 @@ namespace Game.Buildings
         public Jobs()
         {
             this.InitializeComponent();
-        }
-        
+        }       
 
         public void BurgerJob()
         {
